8a880d8f13a7d5b6ee27888bcda364162192db55
seastar <2229029156@qq.com>
2023-02-01 19:34:00 +0800

    update NDArrayUtils.java

--- src/main/java/utils/cv/NDArrayUtils.java
+++ src/main/java/utils/cv/NDArrayUtils.java
@@ -1,11 +1,17 @@
 package utils.cv;
 
 import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDManager;
+import ai.djl.modality.cv.Image;
+import org.bytedeco.javacpp.indexer.ByteIndexer;
 import org.bytedeco.javacpp.indexer.DoubleRawIndexer;
+import org.bytedeco.javacpp.indexer.UByteRawIndexer;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Point2f;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.util.List;
 
 /**
@@ -87,4 +93,50 @@ public class NDArrayUtils {
 
         return points2f;
     }
+
+    /**
+     * bufferedimage to mat
+     * @param img
+     * @return
+     */
+    public static Mat image2Mat(Image img) {
+        var bufferedImage = (BufferedImage) img.getWrappedImage();
+        var width = bufferedImage.getWidth();
+        var height = bufferedImage.getHeight();
+        byte[] data = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+        var mat = new Mat(height, width, opencv_core.CV_8UC3);
+        ByteIndexer byteIndexer = mat.createIndexer();
+        byteIndexer.put(0, 0, data);
+        return mat;
+    }
+
+    /**
+     * bufferedimage to mat
+     * @param img
+     * @return
+     */
+    public static Mat image2Mat2(Image img) {
+        Mat mat = null;
+        try(NDManager manager = NDManager.newBaseManager()) {
+            var ndArray = img.toNDArray(manager);
+            mat = toOpenCVMatV2(ndArray, img.getHeight(), img.getWidth());
+        }
+        return mat;
+    }
+
+    /**
+     *  NDArray to mat
+     * @param points
+     * @param rows
+     * @param cols
+     * @return
+     */
+    public static Mat toOpenCVMatV2(NDArray points, int rows, int cols) {
+        int[] intArray = points.toUint8Array();
+        var mat = new Mat(rows, cols, opencv_core.CV_8UC3);
+        UByteRawIndexer uByteRawIndexer = mat.createIndexer();
+        uByteRawIndexer.put(0, 0, intArray);
+        return mat;
+    }
+
 }
